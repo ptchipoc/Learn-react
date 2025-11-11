4bc8618213d0ae460eaae6d1b2349b03cb9e5d68
ptchipoc <pedrotchipoco@gmail.com>
2025-11-11 05:31:28 +0100

    Updates links to match the router

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export function HomePage() {
       <title>Ecommerce Project</title>
       <div className="header">
         <div className="left-section">
-          <a href="index.html" className="header-link">
+          <a href="/" className="header-link">
             <img className="logo"
               src="images/logo-white.png" />
             <img className="mobile-logo"
@@ -24,7 +24,7 @@ export function HomePage() {
         </div>
 
         <div className="right-section">
-          <a className="orders-link header-link" href="orders.html">
+          <a className="orders-link header-link" href="/hordersl">
 
             <span className="orders-text">Orders</span>
           </a>
