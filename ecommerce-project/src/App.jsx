5acdccf223cb6dbad8ad2a0d3d4e03a7fc991f44
ptchipoc <pedrotchipoco@gmail.com>
2025-11-12 08:44:11 +0100

    test

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
@@ -6,11 +9,19 @@ import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((response) => {
+        setCart(response.data)
+    })
+  }, [])
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path='checkout' element={<CheckoutPage />}  />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />}  />
       <Route path='orders' element={<OrdersPage />} />
       <Route path='tracking' element={<TrackingPage />} />
     </Routes>
