ffe859514b157c15ddea1a3ea8197c0b20dd4eac
ptchipoc <pedrotchipoco@gmail.com>
2025-11-15 10:58:19 +0100

    Use beforeEach to remove doplication

--- ecommerce-project/src/pages/Home/Product.test.jsx
+++ ecommerce-project/src/pages/Home/Product.test.jsx
@@ -1,6 +1,6 @@
 import userEvent from '@testing-library/user-event'
 import axios from 'axios'
-import {it, expect, describe, vi} from 'vitest'
+import {it, expect, describe, vi, beforeEach} from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { Product } from './Product'
 
@@ -8,20 +8,26 @@ vi.mock('axios')
 
 describe ('Product component', () => {
 
-  it('Displays the product details currectly', () => {
-    const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-    };
+  let product; 
+  let loadCart;
 
-    const loadCart = vi.fn();
+  beforeEach(() => {
+      product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"]
+    };
+    loadCart = vi.fn();
+
+  })
+  it('Displays the product details currectly', () => {
+    
     render(<Product product={product} loadCart={loadCart} />)
     
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
@@ -32,19 +38,7 @@ describe ('Product component', () => {
   })
 
   it ('adds a product to the cart', async () => {
-    const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-    };
-
-    const loadCart = vi.fn();
+    
     render(<Product product={product} loadCart={loadCart} />)
 
     const user = userEvent.setup();
