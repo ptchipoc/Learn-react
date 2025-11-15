6e2624feb77d5f992ca99841434375a4f83cdf9f
ptchipoc <pedrotchipoco@gmail.com>
2025-11-15 11:32:40 +0100

    Create Integration test for HomePage

--- ecommerce-project/src/pages/Home/Product.jsx
+++ ecommerce-project/src/pages/Home/Product.jsx
@@ -19,7 +19,10 @@ export function Product({ product, loadCart }) {
   }
 
   return (
-    <div className="product-container">
+    <div
+      className="product-container"
+      data-testId="product-container"
+    >
       <div className="product-image-container">
         <img className="product-image"
           src={product.image}
